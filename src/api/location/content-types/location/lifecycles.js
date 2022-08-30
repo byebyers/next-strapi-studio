1e06c905f9a6ffe3b58cd66f07f4262c1bb8f307
byebyers <byers.jacob@gmail.com>
2022-08-29 17:14:17 -0700

    fix: geocode

--- src/api/location/content-types/location/lifecycles.js
+++ src/api/location/content-types/location/lifecycles.js
@@ -2,23 +2,18 @@ const Geocodio = require('geocodio-library-node');
 const geocoder = new Geocodio(process.env.GEOCODE_API);
 
 
-module.exports = {
-  afterUpdate(event) {
-    const { data, where, select, populate } = event.params;
-    
-    if (event.result.info.address) {
-      event.result.info.details = geocoder.geocode(event.result.info.address)
-        .then(response => {
-          event.result.info.details = response.results[0]
-          console.log(event.result.info)
-        })
-        .catch(err => {
-          console.error(err);
-        }
-      );
-    }
 
-    
-    
+
+module.exports = {
+  beforeCreate(event) {
+    event.params.data.details = geocoder.geocode(event.params.data.address)
+      .then(response => {
+        event.params.data.details = response.results[0]
+        console.log(event.params.data.details)
+      })
+      .catch(err => {
+        console.error(err);
+      }
+    );
   },
 };
