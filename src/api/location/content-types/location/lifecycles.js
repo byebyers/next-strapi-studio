5875975a7b279806f32fcf91ce93343713f9b2c8
byebyers <byers.jacob@gmail.com>
2022-08-31 12:33:42 -0700

    fix: geocode backend & better Vercel hook

--- src/api/location/content-types/location/lifecycles.js
+++ src/api/location/content-types/location/lifecycles.js
@@ -1,19 +1,39 @@
-const Geocodio = require('geocodio-library-node');
-const geocoder = new Geocodio(process.env.GEOCODE_API);
+const NodeGeocoder = require('node-geocoder');
 
+const options = {
+  provider: 'openstreetmap'
+};
+
+const geocoder = NodeGeocoder(options);
+
+
+// module.exports = {
+//   beforeCreate(event) {
+//     event.params.data.details = geocoder.geocode(event.params.data.address)
+//       .then(response => {
+//         event.params.data.details = response.results[0]
+//         console.log(event.params.data.details)
+//       })
+//       .catch(err => {
+//         console.error(err);
+//       }
+//     );
+//   },
+// };
 
 
 
 module.exports = {
-  beforeCreate(event) {
-    event.params.data.details = geocoder.geocode(event.params.data.address)
-      .then(response => {
-        event.params.data.details = response.results[0]
-        console.log(event.params.data.details)
-      })
-      .catch(err => {
-        console.error(err);
-      }
-    );
+  async beforeCreate(event) {
+    const { data } = event.params;
+    const res = await geocoder.geocode(`${data.address}`)
+    data.latitude = res[0].latitude;
+    data.longitude = res[0].longitude;
+  },
+  async beforeUpdate(event) {
+    const { data } = event.params;
+    const res = await geocoder.geocode(`${data.address}`)
+    data.latitude = res[0].latitude;
+    data.longitude = res[0].longitude;
   },
 };
