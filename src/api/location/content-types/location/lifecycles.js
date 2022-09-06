b90a306a995214652693f51c585c9b074e236a64
byebyers <byers.jacob@gmail.com>
2022-09-06 14:12:22 -0700

    fix: slug to lowercase

--- src/api/location/content-types/location/lifecycles.js
+++ src/api/location/content-types/location/lifecycles.js
@@ -28,15 +28,17 @@ module.exports = {
   async beforeCreate(event) {
     const { data } = event.params;
     const res = await geocoder.geocode(`${data.address}`)
+    const slug = slugify(data.title.toLowerCase())
     data.latitude = res[0].latitude;
     data.longitude = res[0].longitude;
-    data.slug = slugify(data.title)
+    data.slug = slug
   },
   async beforeUpdate(event) {
     const { data } = event.params;
     const res = await geocoder.geocode(`${data.address}`)
+    const slug = slugify(data.title.toLowerCase())
     data.latitude = res[0].latitude;
     data.longitude = res[0].longitude;
-    data.slug = slugify(data.title)
+    data.slug = slug
   },
 };
